73f0a42e117120b4976130819d22897280f8bef4
Samuel Lampa <samuel.lampa@gmail.com>
2017-06-20 10:23:16 +0200

    Updates in timing script

--- perftest/testperf.sh
+++ perftest/testperf.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-for n in 100 200 300 400 500 600 700 800 900 1000 2000 3000 4000 5000 6000 7000 8000 9000 10000; do
-	echo "--------------------------------------------------------------------------------" >> import_log.txt
-	echo "Starting to import $n triples ..." >> import_log.txt;
-	echo "--------------------------------------------------------------------------------" >> import_log.txt
+for n in 100 1000 10000; do
+	now=$(date "+%Y-%m-%d.%H:%M")
+	echo "--------------------------------------------------------------------------------" | tee -a import_log.txt
+	echo "$now: Starting to import $n triples ..." | tee -a import_log.txt;
+	echo "--------------------------------------------------------------------------------" | tee -a import_log.txt
 	./restoredb.sh
 	sleep 1
-	{ /usr/bin/time -f '%e seconds' php rdfio-maintenance/importRdf.php --indata "testperf_"$n".nt"; } &>> import_log.txt
+	{ /usr/bin/time -f '%e s' php rdfio-maintenance/importRdf.php --chunksize 100 --indata "testperf_"$n".nt"; } 2>&1 | tee -a import_log.txt
 	sleep 1
 done;
 
